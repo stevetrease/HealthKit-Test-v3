a5d050443000f05f44caa18363d7107ae7678b0a
Steve Trease <steve.trease@shell.com>
2017-06-24 10:19:36 +0100

    Initial Commit

---
+++ HealthKit-Test/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HealthKit-Test
+//
+//  Created by Steve on 24/06/2017.
+//  Copyright © 2017 Steve. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
