de68ad97e553cc5adb91b0453cc4d6d37ff2f2ef
Steve Trease <steve@broadband.bt.com>
2017-09-04 11:08:10 +0100

    rightLabel blank before refresh

--- HealthKit-Test/CustomTableViewHeaderCell.swift
+++ HealthKit-Test/CustomTableViewHeaderCell.swift
@@ -16,6 +16,8 @@ class CustomTableViewHeaderCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
+        
+        rightLabel.text = ""
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
